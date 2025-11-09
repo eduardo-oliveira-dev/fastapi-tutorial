dd4467ad26731cef8e052664920ad92f90fb437b
Eduardo Oliveira <eduoliveira5002@gmail.com>
2025-11-09 16:35:19 -0300

    Add GET /posts endpoint with sample test data

---
+++ app/app.py
@@ -0,0 +1,9 @@
+from fastapi import  FastAPI
+
+app = FastAPI()
+
+text_posts = {1: {"title": "New Post", "content": "cool test post"}}
+
+@app.get("/posts")
+def get_all_posts():
+    return text_posts
