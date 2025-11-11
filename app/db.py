7b308521631550b048974a2229bccef9f0b81bcf
Eduardo Oliveira <eduoliveira5002@gmail.com>
2025-11-11 12:18:53 -0300

    Add user authentication and management features with FastAPI Users integration; update database schema for user and post relationships

--- app/db.py
+++ app/db.py
@@ -2,26 +2,37 @@ from collections.abc import AsyncGenerator
 
 import uuid
 
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
 from sqlalchemy import Column, String, DateTime, ForeignKey, Text
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine,async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
 from datetime import datetime
+from fastapi_users.db import SQLAlchemyUserDatabase, SQLAlchemyBaseUserTableUUID
 
 DATABASE_URL = "sqlite+aiosqlite:///test.db"
 
 class Base(DeclarativeBase):
     pass
 
-class Post(Base):
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    posts = relationship(argument="Post", back_populates="user")
+
+
+class Post(Base):  # 6 usages
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
     caption = Column(Text)
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user = relationship(argument="User", back_populates="posts")
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
@@ -33,3 +44,7 @@ async def create_db_tables():
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User)
